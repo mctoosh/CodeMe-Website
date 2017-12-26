d56b5e1ae9251f5e3e756452702639d67f8b13e7
Abdinasir Hussein <P15200790@LEC-ADMIN.dmu.ac.uk>
2017-12-26 10:59:17 +0000

    I have added the google api and also made it responsive in the contact page

--- js/main.js
+++ js/main.js
@@ -14,4 +14,22 @@ function carousel() {
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
 
-//This code is for the typewriter
+//This code is for the Google Maps API
+"use strict";
+function initMap() {
+  let leicester = {lat: 52.659259, lng: -1.068952};
+  let codeMe = {lat: 52.659259, lng: -1.068952};
+  let address = document.getElementById("map");
+
+  //this will show the city location of the company
+  let map = new google.maps.Map(address, {
+    zoom: 13,
+    center: leicester
+  });
+  //this will show the exact location of the company
+  let markerCodeMe = new google.maps.Marker({
+    position: codeMe,
+    map: map,
+    title: 'The Company'
+  });
+}
